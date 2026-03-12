f2aa38685ebd4bf11104038a8ae18b6a8b1068fb
Tejbeer Anand <tejbeer2006@gmail.com>
2026-03-13 00:46:43 +0530

    .

---
+++ tempcoderunningfile.py
@@ -0,0 +1,12 @@
+
+name="tejbeer"
+# print('s' in name)
+# print('z' in name)
+
+# for i in range(1,6):
+#     print(i)
+    
+# for i in range(6):
+#     print(i)
+# for i in range(2,6):
+#     print(i)
